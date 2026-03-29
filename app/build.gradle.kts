6d04e0e0301fd6537203ae8e219fd3378e52766f
Hector Fabio Romero Bocanegra <hector.f.romero28@gmail.com>
2026-03-29 13:34:52 -0500

    feat: implement Firebase AI SDK and Google Services

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -55,6 +56,10 @@ dependencies {
     // Coil
     implementation(libs.coil.compose)
     implementation(libs.coil.network.okhttp)
+
+    // Google Services - Firebase AI Logic
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.ai)
 
     // Testing
     testImplementation(libs.junit)
--- app/src/main/java/com/hector/firebasebwai26/presentation/home/HomeScreenViewModel.kt
+++ app/src/main/java/com/hector/firebasebwai26/presentation/home/HomeScreenViewModel.kt
@@ -1,6 +1,9 @@
 package com.hector.firebasebwai26.presentation.home
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.ai.ai
+import com.google.firebase.ai.type.GenerativeBackend
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -12,6 +15,8 @@ data class HomeUiState(
 class HomeViewModel() : ViewModel() {
     private val _uiState = MutableStateFlow(HomeUiState())
     val uiState = _uiState.asStateFlow()
+
+    private val model = Firebase.ai(backend = GenerativeBackend.googleAI()).generativeModel("gemini-2.5-flash-lite")
 
     fun increaseCounter() {
         val newValue = _uiState.value.counter + 1
