a886c5823c01c8210480a8d819578e52f01e3055
Hector Fabio Romero Bocanegra <hector.f.romero28@gmail.com>
2026-04-05 12:08:28 -0500

    feat: create basic chat with Gemini

--- app/src/main/java/com/hector/firebasebwai26/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/hector/firebasebwai26/presentation/home/HomeScreen.kt
@@ -3,8 +3,12 @@ package com.hector.firebasebwai26.presentation.home
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -13,16 +17,21 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 fun HomeScreen(modifier: Modifier = Modifier, homeViewModel: HomeViewModel = viewModel()) {
 
     val uiState by homeViewModel.uiState.collectAsStateWithLifecycle()
+    var userPrompt by remember { mutableStateOf("") }
 
     Column(modifier = modifier) {
         Text("Bienvenidos al Workshop GDG 2026")
 
-        Text(text = uiState.counter.toString())
+        TextField(value = userPrompt, onValueChange = { newValue ->
+            userPrompt = newValue
+        })
 
         Button(onClick = {
-            homeViewModel.increaseCounter()
+            homeViewModel.chatWithGemini(userPrompt)
         }) {
-            Text("Aumentar contador")
+            Text("Preguntarle a Gemini")
         }
+
+        Text("Respuesta de Gemini: ${uiState.geminiMessage}")
     }
 }
--- app/src/main/java/com/hector/firebasebwai26/presentation/home/HomeScreenViewModel.kt
+++ app/src/main/java/com/hector/firebasebwai26/presentation/home/HomeScreenViewModel.kt
@@ -1,22 +1,42 @@
 package com.hector.firebasebwai26.presentation.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.firebase.Firebase
 import com.google.firebase.ai.ai
 import com.google.firebase.ai.type.GenerativeBackend
+import com.google.firebase.ai.type.content
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 data class HomeUiState(
-    val counter: Int = 0
+    val counter: Int = 0,
+    val geminiMessage: String? = ""
 )
 
 class HomeViewModel() : ViewModel() {
     private val _uiState = MutableStateFlow(HomeUiState())
     val uiState = _uiState.asStateFlow()
 
-    private val model = Firebase.ai(backend = GenerativeBackend.googleAI()).generativeModel("gemini-2.5-flash-lite")
+    private val model = Firebase.ai(backend = GenerativeBackend.googleAI())
+        .generativeModel(
+            "gemini-2.5-flash-lite",
+            systemInstruction = content {
+                text("Interactuarás con personas que viven en la ciudad de Cali, Colombia. Considera las palabras coloquiales y lugares famosos de la ciudad cuando el usuario te hable.")
+            }
+        )
+
+    private val chat = model.startChat()
+
+    fun chatWithGemini(prompt: String) {
+        viewModelScope.launch {
+            val rawResponse = chat.sendMessage(prompt)
+
+            _uiState.update { it.copy(geminiMessage = rawResponse.text) }
+        }
+    }
 
     fun increaseCounter() {
         val newValue = _uiState.value.counter + 1
